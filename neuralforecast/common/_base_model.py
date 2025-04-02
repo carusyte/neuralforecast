a6ca98b1fa5dacbdfe8b89e57f604a30d7a714da
Jonathan <carusyte@163.com>
2025-04-02 11:18:37 +0800

    enable lr_find to non-auto model

--- neuralforecast/common/_base_model.py
+++ neuralforecast/common/_base_model.py
@@ -21,6 +21,7 @@ import pytorch_lightning as pl
 import neuralforecast.losses.pytorch as losses
 
 from ..losses.pytorch import BasePointLoss, DistributionLoss
+from pytorch_lightning.tuner.tuning import Tuner
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from neuralforecast.tsdataset import (
     TimeSeriesDataModule,
@@ -539,6 +540,15 @@ class BaseModel(pl.LightningModule):
         val_check_interval = min(self.val_check_steps, self.max_steps)
         self.trainer_kwargs["val_check_interval"] = int(val_check_interval)
         self.trainer_kwargs["check_val_every_n_epoch"] = None
+        
+        enable_lr_find = None
+        if "enable_lr_find" in self.trainer_kwargs:
+            enable_lr_find = self.trainer_kwargs.pop("enable_lr_find")
+            if enable_lr_find:
+                trainer = pl.Trainer(**self.trainer_kwargs)
+                tuner = Tuner(trainer)
+                lr_finder = tuner.lr_find(self, datamodule=datamodule)
+                self.learning_rate = lr_finder.suggestion()
 
         if is_local:
             model = self
